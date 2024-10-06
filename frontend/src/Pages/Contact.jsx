dd7f46d58dd3e359446d27bcc91c232f0fb2e576
sumansauravmay <sumansauravmay12345@gmail.com>
2024-10-06 22:30:38 +0530

    contact

--- frontend/src/Pages/Contact.jsx
+++ frontend/src/Pages/Contact.jsx
@@ -1,11 +1,194 @@
-import React from 'react'
+import React from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Text,
+  Stack,
+  Textarea,
+  Tooltip,
+  useClipboard,
+  useColorModeValue,
+  VStack,
+} from "@chakra-ui/react";
+import { BsLinkedin, BsPerson } from "react-icons/bs";
+import { MdEmail, MdOutlineEmail } from "react-icons/md";
+import { FaMobileAlt } from "react-icons/fa";
 
 const Contact = () => {
-  return (
-    <div>
-      Contact
-    </div>
-  )
-}
 
-export default Contact
+  const email = "sonu@nexacodesoftsolution.com";
+  const phoneNumber = "7908627407";
+
+
+
+  const { hasCopied: hasCopiedEmail, onCopy: onCopyEmail } =
+    useClipboard(email);
+  const { hasCopied: hasCopiedPhone, onCopy: onCopyPhone } =
+    useClipboard(phoneNumber);
+
+  return (
+    <Flex
+      bg={useColorModeValue("gray.100", "gray.900")}
+      align="center"
+      justify="center"
+      css={{
+        background: "#9DC4FB",
+        backgroundAttachment: "fixed",
+      }}
+      id="contact"
+    >
+      <Box
+        borderRadius="lg"
+        m={{ base: 5, md: 16, lg: 10 }}
+        p={{ base: 5, lg: 16 }}
+      >
+        <Box>
+          <VStack spacing={{ base: 4, md: 8, lg: 20 }}>
+            <Heading
+              fontSize={{
+                base: "4xl",
+                md: "5xl",
+              }}
+            >
+              Get in Touch
+            </Heading>
+
+            <Stack
+              spacing={{ base: 4, md: 8, lg: 20 }}
+              direction={{ base: "column", md: "row" }}
+            >
+              <Stack
+                align="center"
+                justify="space-around"
+                direction={{ base: "row", md: "column" }}
+              >
+                <Tooltip
+                  label={hasCopiedEmail ? "Email Copied!" : "Copy Email"}
+                  closeOnClick={false}
+                  hasArrow
+                >
+                  <IconButton
+                    aria-label="email"
+                    variant="ghost"
+                    size="lg"
+                    fontSize="3xl"
+                    icon={<MdEmail />}
+                    _hover={{
+                      bg: "blue.500",
+                      color: useColorModeValue("white", "gray.700"),
+                    }}
+                    onClick={onCopyEmail}
+                    isRound
+                  />
+                </Tooltip>
+
+                <Tooltip
+                  label={hasCopiedPhone ? "Number Copied!" : "Copy Number"}
+                  closeOnClick={false}
+                  hasArrow
+                >
+                  <IconButton
+                    aria-label="phone number"
+                    variant="ghost"
+                    size="lg"
+                    fontSize="3xl"
+                    icon={<FaMobileAlt />}
+                    _hover={{
+                      bg: "blue.500",
+                      color: useColorModeValue("white", "gray.700"),
+                    }}
+                    onClick={onCopyPhone}
+                    isRound
+                  />
+                </Tooltip>
+
+                <Box as="a" href="https://www.linkedin.com/">
+                  <IconButton
+                    aria-label="linkedin"
+                    variant="ghost"
+                    size="lg"
+                    icon={<BsLinkedin size="28px" />}
+                    _hover={{
+                      bg: "blue.500",
+                      color: useColorModeValue("white", "gray.700"),
+                    }}
+                    isRound
+                  />
+                </Box>
+              </Stack>
+
+              <Box
+                bg={useColorModeValue("white", "gray.700")}
+                borderRadius="lg"
+                p={8}
+                color={useColorModeValue("gray.700", "whiteAlpha.900")}
+                shadow="base"
+              >
+                <VStack spacing={5}>
+                  <FormControl isRequired>
+                    <FormLabel>Name</FormLabel>
+
+                    <InputGroup>
+                      <InputLeftElement>
+                        <BsPerson />
+                      </InputLeftElement>
+                      <Input type="text" name="name" placeholder="Your Name" />
+                    </InputGroup>
+                  </FormControl>
+
+                  <FormControl isRequired>
+                    <FormLabel>Email</FormLabel>
+
+                    <InputGroup>
+                      <InputLeftElement>
+                        <MdOutlineEmail />
+                      </InputLeftElement>
+                      <Input
+                        type="email"
+                        name="email"
+                        placeholder="Your Email"
+                      />
+                    </InputGroup>
+                  </FormControl>
+
+                  <FormControl isRequired>
+                    <FormLabel>Message</FormLabel>
+
+                    <Textarea
+                      name="message"
+                      placeholder="Your Message"
+                      rows={6}
+                      resize="none"
+                    />
+                  </FormControl>
+
+                  <Button
+                    colorScheme="blue"
+                    bg="blue.400"
+                    color="white"
+                    _hover={{
+                      bg: "blue.500",
+                    }}
+                    width="full"
+                  >
+                    Send Message
+                  </Button>
+                </VStack>
+              </Box>
+            </Stack>
+          </VStack>
+        </Box>
+      </Box>
+    </Flex>
+  );
+};
+
+export default Contact;
