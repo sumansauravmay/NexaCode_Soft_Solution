031ea854ad24be7dc6972249b9a227771f00d8ec
sumansauravmay <sumansauravmay12345@gmail.com>
2024-10-06 17:56:24 +0530

    navbar

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Allroutes from './Allroutes/Allroutes';
+
+function App() {
+  return (
+    <div>
+     <Navbar/>
+     <Allroutes/>
+    </div>
+  );
+}
+
+export default App;
